d3e3db70237f19fc70169a280513060b6c2af265
michaelsnow3 <mikesnow444@gmail.com>
2019-03-29 12:05:50 -0400

    added settings icon to board display page

--- src/components/DisplayBoard/DisplayBoard.js
+++ src/components/DisplayBoard/DisplayBoard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import './DisplayBoard.css';
+import settings from '../../settings.png'
 
 // import components
 import List from '../List/List';
@@ -21,11 +22,18 @@ function DisplayBoard({ activeBoard, boardLists }) {
     return acc;
   }, []);
 
+  const handleSettingsClick = () => {
+    console.log('gear clicked')
+  }
+
   boardListComponents.push(<AddList boardId={boardId} key={-1}/>)
 
   return (
     <div>
-      <div className="boardTitle">{boardTitle}</div>
+      <div className="boardHeader">
+        <div className="boardTitle">{boardTitle}</div>
+        <img onClick={handleSettingsClick} className="boardSettings" src={settings} alt='gear' />
+      </div>
       <div className="boardLists">{boardListComponents}</div>
     </div>
   );
