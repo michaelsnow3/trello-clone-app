cc9b002a37134d13f03afa3cd02efb489430dbf9
michaelsnow3 <mikesnow444@gmail.com>
2019-03-29 15:16:34 -0400

    redirects to users boards page after table delete

--- src/components/dropdownMenu/BoardMenu.js
+++ src/components/dropdownMenu/BoardMenu.js
@@ -1,16 +1,55 @@
-import React from 'react'
-import {Dropdown, DropdownButton} from 'react-bootstrap'
+import React, {useState} from 'react';
+import { connect } from 'react-redux';
+import { Dropdown, DropdownButton } from 'react-bootstrap';
+import { Redirect } from 'react-router-dom';
 
-const BoardMenu = () => {
-  return(
+const mapStateToProps = state => {
+  return {
+    activeBoard: state.boardInfo.activeBoard,
+    username: state.userInfo.username
+  };
+};
+
+const BoardMenu = ({ activeBoard, username }) => {
+  let [redirect, setRedirect] = useState(false)
+  const handleBoardEdit = () => {
+    console.log('edit');
+  };
+  const handleBoardDelete = () => {
+    let boardId = activeBoard.id;
+    fetch(`http://localhost:8888/board/del/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        boardId
+      })
+    })
+      .then(() => {
+        setRedirect(true);
+      })
+      .catch(error => console.log('error deleting board', error));
+  };
+
+  const renderRedirect = () => {
+    if (redirect) {
+      return <Redirect to={`/${username}/boards`} />;
+    }
+  }
+
+  return (
     <div>
-      <DropdownButton id="dropdown-basic-button" title='Edit'>
-        <Dropdown.Item href="#/action-1">Edit Board Title</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Delete Board</Dropdown.Item>
+      {renderRedirect()}
+      <DropdownButton id="dropdown-basic-button" title="Settings">
+        <Dropdown.Item onClick={handleBoardEdit}>Edit Title</Dropdown.Item>
+        <Dropdown.Item onClick={handleBoardDelete}>Delete Board</Dropdown.Item>
       </DropdownButton>
     </div>
-  )
-}
+  );
+};
 
-
-export default BoardMenu
+export default connect(
+  mapStateToProps,
+  null
+)(BoardMenu);
