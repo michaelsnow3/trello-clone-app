b87eafe69d2e57162d8c0fd67f48d35d4d73cd38
michaelsnow3 <mikesnow444@gmail.com>
2019-04-02 17:32:30 -0400

    boards page redirects to login page if user is not logged in

--- src/components/ListBoards/ListBoards.js
+++ src/components/ListBoards/ListBoards.js
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 import './ListBoards.css';
 
@@ -26,6 +27,18 @@ function ListBoards({ boards, getUserBoards, userId, isPending }) {
   useEffect(() => {
     getUserBoards(userId);
   }, [userId, getUserBoards]);
+  let [redirect, setRedirect] = useState(false);
+
+  // redirect to login if no userId exists
+  if (!userId && !redirect) {
+    setRedirect(true);
+  }
+
+  const renderRedirect = () => {
+    if (redirect) {
+      return <Redirect to={`/login/`} />;
+    }
+  };
 
   // return loading until user boards have been fetched
   if (isPending) {
@@ -34,11 +47,14 @@ function ListBoards({ boards, getUserBoards, userId, isPending }) {
 
   let userBoards = [<AddBoard key={-1} />];
 
-  boards.forEach((board, i) => {
-    userBoards.push(<Board board={board} key={i} />);
-  });
+  if (userId) {
+    boards.forEach((board, i) => {
+      userBoards.push(<Board board={board} key={i} />);
+    });
+  }
   return (
     <div>
+      {renderRedirect()}
       <div className="boardListContainer">{userBoards}</div>
     </div>
   );
