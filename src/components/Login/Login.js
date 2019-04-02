19b9759e1f801e19fac5b3b96f77c4559cd66540
michaelsnow3 <mikesnow444@gmail.com>
2019-04-01 21:14:03 -0400

    displays alert message if incorrect login info

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import Button from 'react-bootstrap/Button';
+import { Button, Alert } from 'react-bootstrap';
 
 import './Login.css';
+
+import { postFetch } from '../../fetchRequests';
 
 // import actions
 import { setUserInfo } from '../../actions/userInfoActions';
@@ -23,28 +25,86 @@ const mapDispatchToProps = dispatch => {
 };
 
 function Login({ username, setUserInfo, getUserBoards }) {
-  const handleLogin = () => {
-    setUserInfo(getUserBoards);
+  const [usernameValue, setUsernameValue] = useState('');
+  const [passwordValue, setPasswordValue] = useState('');
+  const [showAlert, setShowAlert] = useState(false);
+
+  const verifyLogin = () => {
+    let body = {
+      username: usernameValue,
+      password: passwordValue
+    };
+    postFetch('/user/login/', body)
+      .then(data => data.json())
+      .then(userInfo => {
+        if (userInfo) {
+          console.log(userInfo);
+          // input correct
+          // setUserInfo(() => getUserBoards(userId))
+        } else {
+          // input incorrect
+          return setShowAlert(true);
+        }
+      });
   };
 
-  return (
+  const loginForm = (
     <div className="loginForm">
-      <h1 className='loginFromTitle'>Login</h1>
+      <h1 className="loginFromTitle">Login</h1>
 
-      <div className='loginInputGroup'>
+      <div className="loginInputGroup">
         <h4 className="loginFormLabel">Username</h4>
-        <input className="loginFormInput" placeholder='username' />
+        <input
+          value={usernameValue}
+          className="loginFormInput"
+          placeholder="username"
+          onChange={event => handleValueChange(event, setUsernameValue)}
+        />
       </div>
 
-      <div className='loginInputGroup'>
+      <div className="loginInputGroup">
         <h4 className="loginFormLabel">Password</h4>
-        <input className="loginFormInput" placeholder='password' />
+        <input
+          value={passwordValue}
+          className="loginFormInput"
+          placeholder="password"
+          type="password"
+          onChange={event => handleValueChange(event, setPasswordValue)}
+        />
       </div>
-      <Button variant="primary" type="submit" className='loginFormButton'>
+      <Button
+        onClick={verifyLogin}
+        variant="primary"
+        type="submit"
+        className="loginFormButton"
+      >
         Submit
       </Button>
     </div>
   );
+
+  const incorrectInputAlert = () => {
+    if (showAlert) {
+      return (
+        <Alert
+          className="loginAlert"
+          dismissible
+          variant="warning"
+          onClose={() => setShowAlert(false)}
+        >
+          <Alert.Heading className='loginAlertHeading'>Incorrect username or password</Alert.Heading>
+        </Alert>
+      );
+    } else {
+      return loginForm;
+    }
+  };
+
+  const handleValueChange = (event, setValue) => {
+    setValue(event.target.value);
+  };
+
+  return <div>{incorrectInputAlert()}</div>;
 }
 
 export default connect(
